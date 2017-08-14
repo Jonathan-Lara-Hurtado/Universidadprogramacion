ab397faac4944d77576a6e28854ad2fa16751425
Jonathan Lara Hurtado <jonabimael@gmail.com>
2017-08-14 17:22:13 -0500

    programa del dia 14 de agosto

---
+++ Matriz.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "lara.h"
+/*
+
+Este programa depende de la libreria lara.h
+
+*/
+
+void main(){
+
+int **A,**B,**C,**D;
+int n=3,m=4;
+
+A=ReservaMatriz(n,m);
+B=ReservaMatriz(n,m);
+C=ReservaMatriz(n,m);
+
+A=MatrizAleatoria(n,m);
+B=MatrizAleatoria(n,m);
+
+C=SumaMatrizpares(A,B,n,m);
+
+D=RestaMatrizpares(A,B,n,m);
+
+printf("Matriz A:\n");
+imprimirMatriz(A,n,m);
+printf("\n");
+
+printf("Matriz B:\n");
+imprimirMatriz(B,n,m);
+printf("\n");
+
+printf("Resultado de la suma de la Matriz A y B:\n");
+imprimirMatriz(C,n,m);
+printf("\n");
+
+printf("Resultado de la resta de la Matriz A y B:\n");
+imprimirMatriz(D,n,m);
+printf("\n");
+
+
+//fibonacci
+
+int k=6;
+int r;
+
+r=Fibonacci(k);
+printf("El resultado de la serie de fibonacci es:%d\n",r);
+
+
+}
+
