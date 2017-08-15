def15d37ac0a95365948e6c7535e3b1002fb470d
Usuario <usuario@IMACUAEM-D28.local>
2017-08-15 08:43:34 -0500

    ajustes de kivy

---
+++ probarki.py
@@ -0,0 +1,55 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    canvas:
+        Color:
+            rgb:1,1,1
+        Rectangle:
+            size:self.size
+            pos:self.pos
+    BoxLayout:
+        size_hint:(None,None)
+ #       size:100,44
+        pos_hint:{"bottom_x":.55,"left_x":.49}
+        Button:
+        Button:
+        Button:
+        Button:
+   # BoxLayout:
+#        Button:
+            
+
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+
+class TestApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
