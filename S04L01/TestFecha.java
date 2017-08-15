d59484a78515673fac3db0bded73644cef3dcc1a
Usuario <usuario@IMACUAEM-D28.local>
2017-08-15 09:13:29 -0500

    15 de agosto de 2017 POO

---
+++ S04L01/TestFecha.java
@@ -0,0 +1,15 @@
+public class TestFecha
+{
+    public static void main(String args[]){
+    FECHA miFecha= new FECHA();
+    
+    miFecha.mes="agosto";
+    miFecha.dia=31;
+    miFecha.anio=1997;
+    
+    
+    miFecha.print();
+}
+    
+    
+}
