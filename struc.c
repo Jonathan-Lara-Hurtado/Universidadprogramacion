62af7f05000b771b769d06c4cc2cbbe1d303c427
Usuario <usuario@IMACUAEM-D28.local>
2017-08-17 13:03:10 -0500

    ejemplo de struct 17 de agosto de 2017

--- struc.c
+++ struc.c
@@ -23,8 +23,18 @@ void main(){
 
 
 
+Alumno *Registro;
+//struc con apuntador 
+Registro=(Alumno*)malloc(sizeof(Alumno));
+Registro->promedio=7.5;
+strcpy(Registro->nombre,"Juan");
+Registro->edad=21;
+Registro->grupo=3;
+
 
 Alumno Lista[3];
+
+
 
 Lista[0].promedio=87.2;
 
@@ -33,7 +43,7 @@ strcpy(Lista[0].nombre,"jonathan");
 Lista[0].edad=20;
 Lista[0].grupo=03;
 
-
+int n=0;
 
 
 
@@ -43,6 +53,17 @@ printf("%f\n",Lista[0].promedio);
 printf("%d\n",Lista[0].edad);
 
 printf("%d\n",Lista[0].grupo);
+
+
+printf("impresion de apuntador struct\n");
+
+printf("%s\n",Registro[0].nombre);
+printf("%f\n",Registro[0].promedio);
+
+printf("%d\n",Registro[0].edad);
+
+printf("%d\n",Registro[0].grupo);
+
 
 
 
