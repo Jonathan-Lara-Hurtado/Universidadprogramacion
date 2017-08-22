f75587d03ccc8d69a9b2e6d3f64a3f623fedf72b
Usuario <usuario@IMACUAEM-D22.local>
2017-08-22 09:31:13 -0500

    agregando estos archivo

--- S04L01/FECHA.java
+++ S04L01/FECHA.java
@@ -2,16 +2,101 @@
 public class FECHA
 {
    public String mes;
-   public int dia;
+   private int dia;
    public int anio;
    
    
+   //manipular atributos protegidos
+   
+   
+   public void setDia(int tmpDia){
+    
+    
+    if(tmpDia>0){
+        
+        if(tmpDia<32)
+        {
+        dia=tmpDia;
+    
+        }else{
+        
+        dia=1;
+        }
+
+    
+    
+     }else{
+        
+        
+        dia=1;
+        }
+    
+    
+    
+    
+    }
+    
+    
+    
+    //get de atributo privato 
+    
+    
+    public int getDia(){
+    
+        return dia;
+    
+    
+    }
+   
+   
+   
+   
+    public void incrementardia(){
+    
+        
+        
+        
+        dia=dia+1;
+    /*
+    
+   
+    switch(m){
+        case "enero":
+        if(dia>30){
+            
+            mes="febrero";
+            
+        }
+        
+        
+        
+    }
+      */  
+    
+    } 
+   
    public void print(){
-    
-    
        System.out.println(mes+""+dia+""+anio);
     }
+    
+    
+    public void printDMY(){
+    
+     System.out.println(dia+"/"+mes+"/"+anio);
+    
+    }
   
+    public void setDate(int d,String m, int a){
+
+        
+        dia= d;
+        mes =m;
+        a= anio;
+    
+    
+    
+    }
+
     
    
 }
--- S04L01/TestFecha.java
+++ S04L01/TestFecha.java
@@ -2,13 +2,20 @@ public class TestFecha
 {
     public static void main(String args[]){
     FECHA miFecha= new FECHA();
-    
-    miFecha.mes="agosto";
-    miFecha.dia=31;
+    miFecha.mes="enero";
+    //miFecha.dia=30;
+    miFecha.setDia(31);
     miFecha.anio=1997;
+    System.out.println("antes de incrementar:");
+    miFecha.printDMY();
+    miFecha.incrementardia();
+    System.out.println("Despues de incrementar:");
+    miFecha.printDMY();
     
+    miFecha.setDate(22,"julio",1993);
     
-    miFecha.print();
+    miFecha.printDMY();
+    
 }
     
     
